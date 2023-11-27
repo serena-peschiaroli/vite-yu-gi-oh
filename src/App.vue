85868dc25bc229dc43439efd73b57ae44600cab4
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-11-27 15:12:24 +0100

    Add: AppHeader; AppCardsList WIP

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,15 @@ import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
 import { store } from "./store.js";
 export default {
+  data(){
+    return{
+      store
+    };
+  },
   components: {
     AppHeader,
   }
-}
+};
 
 
 </script>
