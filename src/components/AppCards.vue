95872dcfd0b70627e10884ab5e872b9c3a7a3482
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-11-27 15:44:17 +0100

    Add: AppCards template, props;  AppCardsLists ms-container, row, col

---
+++ src/components/AppCards.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    props: {
+        card: Object,
+    }
+}
+</script>
+
+<template>
+    <div class="card" >
+        <div class="card-body">
+            <img :src=" card.card_images[2].image_url" alt=""/>
+            <h3 class="card-title"> {{ card.name }}</h3>
+            <p class="card-text"> {{ card.archetype }}</p>
+           
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+</style>
--- src/components/AppCardsList.vue
+++ src/components/AppCardsList.vue
@@ -1,9 +1,20 @@
 <script>
+
 </script>
 
 <template>
+    <div class="row">
+        <div class="col">
+            
+
+        </div>
+    </div>
 
 </template>
 
 <style lang="scss" scoped>
+.ms_container {
+    width: 90%;
+    margin: 0 auto;
+}
 </style>
