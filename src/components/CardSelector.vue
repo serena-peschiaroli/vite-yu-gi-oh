2314b7309fe3be183ff098d9a28f0af68aa5ccd8
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-11-28 17:14:16 +0100

    WIP: cardCount

--- src/components/CardSelector.vue
+++ src/components/CardSelector.vue
@@ -1,5 +1,17 @@
 <script>
 export default {
+    // props per conteggio  carte
+    props : {
+        cardsCount : {
+            type: Number,
+            default: 0,
+        },
+        //prop per ricevere filtro 
+        currentFilter: {
+            type: String,
+            defaul: '',
+        },
+    },
     data() {
         return {
             selectedArchetype: '',
@@ -13,23 +25,56 @@ export default {
             //notifica componente genitore sull'archetipo selezionato
             this.$emit("filter-change", this.selectedArchetype);
         },
+        //methods x gestire cambio nel filtro corrente 
+        methods: {
+            handleCurrentFilterChange(newfilter) {
+                //if conteggio solo se il filtro corrente coincide con l'archetipo
+                this.$emit("aggiorno il numero di carte", this.cardsCount);
+            }
+        },
+        //aggiungo un hook x chiamare il metodo quando il componente è creato
+        created() {
+            this.handleCurrentFilterChange(this.currentFilter);
+        },
 
     },
 };
 </script>
 
 <template>
-    <div>
-        <label for="archetype">Select Archetype:</label>
-        <select v-model="selectedArchetype">
-        <option v-for="archetype in archetypes" :key="archetype" :value="archetype">
-            {{ archetype }}
-        </option>
-    </select>
-    <button class="btn btn-primary" @click="emitFilterEvent"> Filtra </button>
-    </div>
+    <section class="d-flex justify-between">
 
+        <div class="input-container">
+            <label for="archetype">Select Archetype:</label>
+            <select v-model="selectedArchetype">
+            <option v-for="archetype in archetypes" :key="archetype" :value="archetype">
+                {{ archetype }}
+            </option>
+            </select>
+            <button class="btn btn-primary" @click="emitFilterEvent"> Filtra </button>
+        </div>
+
+        <div class="count">
+            {{ cardsCount }} carte trovate
+
+        </div>
+    </section>
 </template>
 
 <style scoped lang="scss">
+@use "../style/general.scss";
+.input-container {
+    width: 40%;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+}
+
+
+
+
+
+
+
 </style>
